6b48685ed32201001bc1d7ebae00ac2664f49935
Rahul Mangalampalli <rahul_mangalampalli@yahoo.in>
2020-07-26 13:36:12 +0530

    documentation updated

--- txt.py
+++ txt.py
@@ -4,14 +4,14 @@ txt = []
 count=0
 dir = 'images/'  #Edit this path to your folder of images.
 labels = []
-for dirname in os.listdir(dir):
-    for labs in os.listdir(dir+'/'+dirname):
-        txt.append(dirname+'/'+labs+' '+str(count)+'\n')
+for dirname in os.listdir(dir):  # List of directories present inside the specified folder which are classes
+    for labs in os.listdir(dir+'/'+dirname):  # List of images containing per class
+        txt.append(dirname+'/'+labs+' '+str(count)+'\n')  # Appending the path of images to a list
     count+=1
-    labels.append(dirname+'\n')  
-random.shuffle(txt)
-with open("train.txt", "a") as file1:
-  file1.writelines(txt[0:int(len(txt)*1)])
+    labels.append(dirname+'\n')  # Append Labels to seperate list
+random.shuffle(txt) # Randomly shuffiling the list
+with open("train.txt", "a") as file1:  # Appending the list to text file.
+  file1.writelines(txt[0:int(len(txt)*1)]) 
 #with open("test_fin.txt", "a") as file1:
 #  file1.writelines(txt[int(len(txt)*0.8):])
 #with open("labels.txt", "a") as file1:
